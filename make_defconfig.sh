654af95e5dab677943136198f12e3f7378a2be68
Devin Kim <DevinKim@gmail.com>
2015-06-07 17:03:11 +0200

    Add make_defconfig script tools for save defconfig $ ./make_defconfig.sh mako-perf_defconfig it will update in arch/arm/configs
    Change-Id: Ic4946b4ac020faf7026bde216b480994ff9a71a8
    
    +Adaptation for Rhine devices, to be updated for shinano.

---
+++ make_defconfig.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for device in honami togari amami; do
+
+DEFCONFIG_FILE=rhine_${device}_defconfig
+
+if [ ! -e arch/arm/configs/$DEFCONFIG_FILE ]; then
+	echo "No such file: arch/arm/configs/$DEFCONFIG_FILE"
+	exit -1
+fi
+
+# make .config
+make ARCH=arm CROSS_COMPILE=arm-eabi- ${DEFCONFIG_FILE} -j8
+
+mv .config arch/arm/configs/${DEFCONFIG_FILE}
+done
+make mrproper -j8
