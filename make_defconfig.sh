9234ed0ade03ec493b5ac5e35344b41b2ad6ba08
airlessproject <vlatko.mitreski@gmail.com>
2015-07-19 23:51:17 +0200

    adapt defconfigs script
    Change-Id: Ic216523c25a3799b201b7f548d5ebe8c3393f0c2

--- make_defconfig.sh
+++ make_defconfig.sh
@@ -4,6 +4,18 @@ for device in honami togari amami; do
 
 DEFCONFIG_FILE=cm_rhine_row_${device}_defconfig
 
+if [ ! -e arch/arm/configs/$DEFCONFIG_FILE ]; then
+	echo "No such file: arch/arm/configs/$DEFCONFIG_FILE"
+	exit -1
+fi
+
+# make .config
+make ARCH=arm CROSS_COMPILE=arm-eabi- ${DEFCONFIG_FILE} -j8
+
+mv .config arch/arm/configs/${DEFCONFIG_FILE}
+done
+make mrproper -j8
+
 for device in sirius castor castor_windy leo aries scorpion scorpion_windy ; do
 
 DEFCONFIG_FILE=cm_shinano_${device}_defconfig
